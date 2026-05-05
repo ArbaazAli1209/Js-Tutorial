b43d663dfe07d11fec44d7bc5e04b334a298bd3f
Arbaaz <arbaaz8158@gmail.com>
2026-05-05 14:24:17 +0530

    static

---
+++ Js_classes_and_oop/staticprop.js
@@ -0,0 +1,26 @@
+class User{
+    constructor (username) {
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+
+    static createId(){
+        return `123`
+    }
+}
+
+const hitesh = new User("Hitesh")
+// console.log(hitesh.createId());      // Does not give access
+
+class Teacher extends User{
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("Iphone", "i@phone.com")
+iphone.logMe();
