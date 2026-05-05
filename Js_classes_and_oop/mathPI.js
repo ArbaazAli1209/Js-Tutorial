39bbb331d17a72892fc4d901186ffb98cf6f3187
Arbaaz <arbaaz8158@gmail.com>
2026-05-05 17:37:17 +0530

    Descriptor

---
+++ Js_classes_and_oop/mathPI.js
@@ -0,0 +1,27 @@
+// console.log(Math.PI);
+
+const descripter = Object.getOwnPropertyDescriptor(Math, "PI")
+// console.log(descripter);
+
+const chai = {
+    name: 'Ginger Chai',
+    price: 250,
+    isAvailable: true,
+
+    orderChai: function(){
+        console.log("Chai nahi bani");
+    }
+}
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+Object.defineProperty(chai, 'name', {
+    // writable: false,
+    enumerable: true
+})
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+for (let [key, value] of Object.entries(chai)) {
+    if (typeof value !== 'function') {
+        console.log(`${key} ; ${value}`);
+    }
+}
